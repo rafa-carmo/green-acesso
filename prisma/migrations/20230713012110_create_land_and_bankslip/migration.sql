dfc44704a109db6fdc9e313927e0d8cb1b99cb0f
Rafael do Carmo <rafaelcarmo143@gmail.com>
2023-07-13 21:03:25 -0300

    database: migration

---
+++ prisma/migrations/20230713012110_create_land_and_bankslip/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE `Land` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `active` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `BankSlip` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name_receiver` VARCHAR(255) NOT NULL,
+    `landId` INTEGER NOT NULL,
+    `value` DOUBLE NOT NULL,
+    `payment_code` VARCHAR(255) NOT NULL,
+    `active` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `BankSlip` ADD CONSTRAINT `BankSlip_landId_fkey` FOREIGN KEY (`landId`) REFERENCES `Land`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
